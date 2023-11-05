a3793e06c68c68c3b61bd99389e6f8905256d3c2
Saragos <john.haberle@gmail.com>
2023-11-04 18:12:59 -0700

    Add files via upload

--- hedark.cmd
+++ hedark.cmd
@@ -6,23 +6,16 @@ action var lootreceived $2 when As you remove your hand from the Darkbox you see
 action var needshealing 1 when Unfortunately, your wounds make it impossible for you to play the Darkbox
 #action var timetofind 1 when Without warning, the Darkbox simply vanishes.
 
-
-var gems1 agate|alexandrite|amber|amethyst|andalusite|aquamarine|bead|beryl|bloodgem|bloodstone|carnelian|chrysoberyl|carnelian|chalcedony
-var gems2 chrysoberyl|chrysoprase|citrine|coral|crystal|diamond|diopside|emerald|egg|eggcase|garnet|gem|goldstone|glossy malachite
-var gems3 (chunk of|some|piece of).*granite|hematite|iolite|ivory|jade|jasper|kunzite|lapis lazuli|malachite stone|minerals|moonstone|morganite|onyx
-var gems4 opal|pearl|pebble|peridot|quartz|ruby|sapphire|spinel|star-stone|(waermodi|lasmodi|sjatmal|lantholite) stones|sunstone|talon|tanzanite|tooth|topaz|tourmaline|tsavorite|turquoise|zircon
-var allgems %gems1|%gems2|%gems3|%gems4|%gems5
-
-var badloot \S+ kelp|\S+ rockweed|\S+ \S+ rockweed|piece of \S+ sharkskin|\S+ root|\S+ flowers|shark's tooth|burlap cloth|felt cloth|bear-pelt moccasins threaded with white leather laces|cobalt-blue leather belt studded with iron|doeskin moccasins threaded with brown leather laces|embossed leather belt with a gold-washed buckle|fawn-brown leather belt decorated with steel studs|green leather eye patch|light grey leather belt studded with circles of polished amber|pleated deep green wool breeches cross-gartered from ankle to knee with brown leather|purple leather eye patch|seal-pelt moccasins threaded with black leather laces|bear tooth necklace strung on a leather thong|gryphon feather necklace strung on a leather thong|leatherfoot steak
-
-var goodloot infuser stone|potency crystal|\S+ powder|.* cloth|.* stack|.* leather|.* bar|.* nugget|.* fragment|.* lump|.* tear|.* shard|.* ingot|.* pebble|.* rock|.* stone|.* boulder|.* deed|bulging pouch|small pouch|%lootkeeplist
-
 var searchlist 8|10|11|12|13|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|37|49|50|51|52|53|54|55|56|77|78|73|74|76|79|80|81|82|83|84|85|86|87|88|89|90
 
 var needshealing 0
 #var timetofind 0
 var startroom 3
+var wins 0
+var keptwins 0
 
+
+  echo lootkeeplist: %lootkeeplist
 gosub STOWALL
 gosub LOCATIONCHECK
 
@@ -56,7 +49,6 @@ FINDDARKBOXLOOP:
 PLAYDARKBOXP:
   pause
 PLAYDARKBOX:
-  math totalattempts add 1
   matchre PLAYDARKBOXP \.\.\.wait|type ahead|stunned|while entangled in a web\.|You don't seem to be able to move
   match BADPLAY You fish around in the unseen depths of the Darkbox but you find nothing and remove your hand in disappointment.
   match GOODPLAY And you find something!
@@ -76,10 +68,14 @@ OUTOFMONEY:
   goto PLAYDARKBOX
 
 GOODPLAY:
+  math totalattempts add 1
+  math totalspent add 200
   gosub HANDLELOOT
   return
   
 BADPLAY:
+  math totalattempts add 1
+  math totalspent add 200
   goto PLAYDARKBOX
   
  
--- helibrary.cmd
+++ helibrary.cmd
@@ -1,10 +1,22 @@
 
-action put #echo >Log Yellow TotalAttempts: %totalattempts -- TotalPrizes: %totalprizes when A good positive attitude never hurts\.
+action put #echo >Log Yellow [%scripttag] TotalAttempts: %totalattempts -- TotalPrizes: %totalprizes -- PrizesKept: %prizeskept.  TotalSpent: %totalspent when A good positive attitude never hurts\.
 action var lodgedstring $0 when You have a .* lodged
 
 if ("%scripttag" != "DARK") then
 {
-  var lootkeeplist cloth lootbag instructions|cloth lootpouch instructions|dark green sack made of dessicated kelp|dark green sack made of desiccated kelp|(fibrous|folded|tiny|carved) .* icon of .*|faintly glowing starstone|glass octahedron|piece of iridescent .* seaglass|potency crystal|sea-blue vial|sea-green potion|small sea serpent totem|tiny iridescent cube|translucent conch shell inlaid with .* swirls|dramatic.*cloak fastened with a.*clasp|ornate \S+ ring|compact .* textbook bound with a .* cover|water-stained treasure map|elaborately dressed .* puppet with .* eyes|miniature .* doll dressed in .* robes|porcelain toy .* clothed in .* vestments|burnished .* wheel painted with colorful sea creatures|sleek .* lootpouch fastened with a .* leviathan|polished .* wand topped with a .* starfish|iridescent .* filaments strung with .* stars|folding .* chair with a .* cushioned seat|miniature polar bear with round .* eyes|.* thigh pouch|.* robes|.* skirt|length of .* fabric|latticed .* picnic basket tied with|slim.*codex|large.*tome patterend with stylized.*sigils|feathery.*fronds|small.*trident|sylized.*wave|tapered crown of miniature.*tridents|lacquered.*band set with.*clownfish|slender.*eyebrow ring dangling.*sigil|fragile.*blossom with delicate.*petals|fragile blown-glass sphere cradled within a twisted.*frame|pilgrim's badge sculpted with the emblem of|sea urchin mirror with.*gliding along the spines|small.*treasure chest set with.*embellishments|competition miniature Merelew soldier wielding a tiny.*trident|repair case of polished.*with.*fittings|seaglass bottle of chilled zuoganaas insulated with.*kelp fronds|.*contract marked with the crest of the Traders' Guild|tarina|.*instructions|.*ice skates|infuser stone|potency crystal|seaglass-tipped arrows|seaglass-tipped bolts
+  var lootkeeplist cloth lootbag instructions|cloth lootpouch instructions|dark green sack made of dessicated kelp|dark green sack made of desiccated kelp|(fibrous|folded|tiny|carved) .* icon of .*|faintly glowing starstone|glass octahedron|piece of iridescent .* seaglass|sea-blue vial|sea-green potion|small sea serpent totem|tiny iridescent cube|translucent conch shell inlaid with .* swirls|dramatic.*cloak fastened with a.*clasp|ornate \S+ ring|compact .* textbook bound with a .* cover|water-stained treasure map|elaborately dressed .* puppet with .* eyes|miniature .* doll dressed in .* robes|porcelain toy .* clothed in .* vestments|burnished .* wheel painted with colorful sea creatures|sleek .* lootpouch fastened with a .* leviathan|polished .* wand topped with a .* starfish|iridescent .* filaments strung with .* stars|folding .* chair with a .* cushioned seat|miniature polar bear with round .* eyes|.* thigh pouch|.* robes|.* skirt|length of .* fabric|latticed .* picnic basket tied with|slim.*codex|large.*tome patterend with stylized.*sigils|feathery.*fronds|small.*trident|sylized.*wave|tapered crown of miniature.*tridents|lacquered.*band set with.*clownfish|slender.*eyebrow ring dangling.*sigil|fragile.*blossom with delicate.*petals|fragile blown-glass sphere cradled within a twisted.*frame|pilgrim's badge sculpted with the emblem of|sea urchin mirror with.*gliding along the spines|small.*treasure chest set with.*embellishments|competition miniature Merelew soldier wielding a tiny.*trident|repair case of polished.*with.*fittings|seaglass bottle of chilled zuoganaas insulated with.*kelp fronds|.*contract marked with the crest of the Traders' Guild|tarina|.*instructions|.*ice skates|infuser stone|potency crystal|seaglass-tipped arrows|seaglass-tipped bolts
+}
+else
+{
+  var gems1 agate|alexandrite|amber|amethyst|andalusite|aquamarine|bead|beryl|bloodgem|bloodstone|carnelian|chrysoberyl|carnelian|chalcedony
+  var gems2 chrysoberyl|chrysoprase|citrine|coral|crystal|diamond|diopside|emerald|egg|eggcase|garnet|gem|goldstone|glossy malachite
+  var gems3 (chunk of|some|piece of).*granite|hematite|iolite|ivory|jade|jasper|kunzite|lapis lazuli|malachite stone|minerals|moonstone|morganite|onyx
+  var gems4 opal|pearl|pebble|peridot|quartz|ruby|sapphire|spinel|star-stone|(waermodi|lasmodi|sjatmal|lantholite) stones|sunstone|talon|tanzanite|tooth|topaz|tourmaline|tsavorite|turquoise|zircon
+  var allgems %gems1|%gems2|%gems3|%gems4|%gems5
+
+  #var badloot \S+ kelp|\S+ rockweed|\S+ \S+ rockweed|piece of \S+ sharkskin|\S+ root|\S+ flowers|shark's tooth|burlap cloth|felt cloth|bear-pelt moccasins threaded with white leather laces|cobalt-blue leather belt studded with iron|doeskin moccasins threaded with brown leather laces|embossed leather belt with a gold-washed buckle|fawn-brown leather belt decorated with steel studs|green leather eye patch|light grey leather belt studded with circles of polished amber|pleated deep green wool breeches cross-gartered from ankle to knee with brown leather|purple leather eye patch|seal-pelt moccasins threaded with black leather laces|bear tooth necklace strung on a leather thong|gryphon feather necklace strung on a leather thong|leatherfoot steak
+
+  var lootkeeplist infuser stone|potency crystal|\S+ powder|.* cloth|.* stack|.* leather|.* bar|.* nugget|.* fragment|.* lump|.* tear|.* shard|.* ingot|.* pebble|.* rock|.* stone|.* boulder|.* deed|bulging pouch|small pouch
 }
 
 var storage 0
@@ -15,20 +27,22 @@ var healbotname Maorn
 var premium -1
 var totalattempts 0
 var totalprizes 0
+var prizeskept 0
+var totalspent 0
 
 if {"$charactername" = "Saragos") then
 {
   var storage haversack
   var healbot YES
   var healbotroom 204
-  var healbotname Maorn
+  var healbotname Aransa
 }
 if {"$charactername" = "Navesi") then
 {
   var storage cylinder
   var healbot YES
   var healbotroom 204
-  var healbotname Maorn
+  var healbotname Aransa
 }
 
 goto HELIBEND
@@ -87,12 +101,14 @@ REMOVELODGEDLOOP:
 
 
 HANDLELOOT:
+  math totalprizes add 1
   if $righthand = "Empty" then gosub SWAP
   gosub TAPSHORTEN $righthand
+  pause .5
   if matchre("%lootreceived", "%lootkeeplist") then
   {
-    put #echo >Log Yellow [%scripttag]: Won %lootreceived!  On the lootlist, keeping.  TotalAttempts: %totalattempts -- TotalPrizes: %totalprizes
-    math totalprizes add 1
+    put #echo >Log Yellow [%scripttag]: Won %lootreceived!  On the lootlist, keeping.  TotalAttempts: %totalattempts -- TotalPrizes: %totalprizes -- PrizesKept: %prizeskept.  TotalSpent: %totalspent
+    math prizeskept add 1
     if (%storage != 0) then gosub PUTITEM %shorttap in my %storage
     else gosub STOWALL
     if $righthand != "Empty" then
@@ -103,7 +119,7 @@ HANDLELOOT:
   }
   else
   {
-    #put #echo >Log [%scripttag]: Won %lootreceived!  Not on the lootlist, dumping.
+    put #echo >Log [%scripttag]: Won %lootreceived!  Not on the lootlist, dumping.
     gosub DUMPITEM %shorttap
   }
   return
