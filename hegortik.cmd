60fff5fd54f9bc358a3571c02b2405e278256fbd
Saragos <john.haberle@gmail.com>
2023-11-06 19:23:41 -0800

    Add files via upload

--- hegortik.cmd
+++ hegortik.cmd
@@ -1,20 +1,22 @@
 var gortikdoor other door
 var gortikstand stand
 
-waitforre went through a curtained silversteel door
-move go %gortikdoor
-gosub LOOKSHOP first %gortikstand
-gosub LOOKSHOP second %gortikstand
-gosub LOOKSHOP third %gortikstand
+waitforre went through a curtained door
+put go curtain door
+delay .1
+gosub LOOKSTAND first
+gosub LOOKSTAND second
+gosub LOOKSTAND third
+pause 2
 exit
 
-LOOKSHOP:
+LOOKSTAND:
   var looklocation $0
   goto LOOKSTANDMAIN
-LOOKSHOPP:
+LOOKSTANDP:
   pause
 LOOKSTANDMAIN:
-  matchre LOOKSHOPP ^\.\.\.wait|^Sorry\, you may only type ahead|^You are still stunned|^You can\'t do that while|^You don\'t seem to be able|Between the ringing in your head
+  matchre LOOKSTANDP ^\.\.\.wait|^Sorry\, you may only type ahead|^You are still stunned|^You can\'t do that while|^You don\'t seem to be able|Between the ringing in your head
   matchre LOOKRETURN On the .* you see (.*)\.
   match RETURN I could not find what you were referring to.  
   match RETURN You see nothing unusual.
@@ -22,8 +24,10 @@ LOOKSTANDMAIN:
   matchwait
 
 LOOKRETURN:
-  put #echo >Log Orange %looklocation: $1
+  put #echo >Log Yellow %looklocation: $1
   return
   
 RETURN:
   return
+
+
