99bdf476e1371542cfdaf87e0195fd50e3324d0c
Saragos <john.haberle@gmail.com>
2023-11-17 16:05:22 -0800

    Add files via upload

--- hegortik.cmd
+++ hegortik.cmd
@@ -20,6 +20,7 @@ LOOKSTANDMAIN:
   matchre LOOKRETURN On the .* you see (.*)\.
   match RETURN I could not find what you were referring to.  
   match RETURN You see nothing unusual.
+  match RETURN There is nothing on there.
   put look on %looklocation stand
   matchwait
 
